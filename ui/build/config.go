e72863fce238b0d26a95b4ee8a162b743d197054
Ido Ben-Hur <idoybh2@gmail.com>
2024-09-19 14:55:12 +0300

    soong: Installclean when WITH_GMS changes
    Change-Id: I0f20cc7be962f0cff8b60cd7e997f3189ce20d62

--- ui/build/config.go
+++ ui/build/config.go
@@ -1178,6 +1178,16 @@ func (c *configImpl) BuildFromTextStub() bool {
 	return !c.buildFromSourceStub
 }
 
+func (c *configImpl) TargetBuildGapps() bool {
+	if v, ok := c.environ.Get("WITH_GMS"); ok {
+		v = strings.TrimSpace(v)
+		if v == "true" {
+			return true
+		}
+	}
+	return false
+}
+
 func (c *configImpl) TargetProduct() string {
 	if v, ok := c.environ.Get("TARGET_PRODUCT"); ok {
 		return v
@@ -1632,6 +1642,10 @@ func (c *configImpl) ProductOut() string {
 
 func (c *configImpl) DevicePreviousProductConfig() string {
 	return filepath.Join(c.ProductOut(), "previous_build_config.mk")
+}
+
+func (c *configImpl) DevicePreviousGappsConfig() string {
+	return filepath.Join(c.ProductOut(), "previous_gapps_config.mk")
 }
 
 func (c *configImpl) KatiPackageMkDir() string {
